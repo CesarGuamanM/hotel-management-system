74c031e80f0bda0b4c7ee69df42081a5ebdafa61
César Guamán <cesarjunior932025@hotmail.com>
2025-07-26 17:37:02 -0500

    feat(modelo): implementar clase Administrador con privilegios de gestión

--- src/main/java/modelo/Administrador.java
+++ src/main/java/modelo/Administrador.java
@@ -1,5 +1,12 @@
 package modelo;
 
-public class Administrador {
-    
+public class Administrador extends Usuario {
+    public Administrador(String username, String password, String nombre) {
+        super(username, password, nombre);
+    }
+
+    @Override
+    public String getTipoUsuario() {
+        return "Administrador";
+    }
 }
