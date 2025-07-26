1b3020c8769a4d38b480cf5aeb25d487325a2436
César Guamán <cesarjunior932025@hotmail.com>
2025-07-26 17:36:03 -0500

    feat(modelo): implementar clase Recepcionista heredando de Usuario

--- src/main/java/modelo/Recepcionista.java
+++ src/main/java/modelo/Recepcionista.java
@@ -1,5 +1,12 @@
 package modelo;
 
-public class Recepcionista {
-    
+public class Recepcionista extends Usuario {
+    public Recepcionista(String username, String password, String nombre) {
+        super(username, password, nombre);
+    }
+
+    @Override
+    public String getTipoUsuario() {
+        return "Recepcionista";
+    }
 }
