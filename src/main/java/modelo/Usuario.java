0a00dcd76b204542474c3f74174d528004404b6e
César Guamán <cesarjunior932025@hotmail.com>
2025-07-26 17:34:32 -0500

    feat(modelo): agregamos la clase base Usuario con atributos comunes

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -1,5 +1,20 @@
 package modelo;
 
-public class Usuario {
-    
+public abstract class Usuario {
+    private String username;
+    private String password;
+    private String nombre;
+
+    public Usuario(String username, String password, String nombre) {
+        this.username = username;
+        this.password = password;
+        this.nombre = nombre;
+    }
+
+    // Getters y setters
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getNombre() { return nombre; }
+
+    public abstract String getTipoUsuario();
 }
